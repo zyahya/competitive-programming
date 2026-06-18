0f3c0c7ee9dae4bbc2d8b54addcd8d73cb9568e6
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-18 12:29:02 +0300

    Add tests for valid palindrome

---
+++ leetcode/csharp/q20_valid_parentheses/q20_valid_parentheses_tests.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Solutions.q20_valid_parentheses;
+
+public class ValidParentheses_Tests
+{
+    [Theory]
+    [InlineData("[()]", true)]
+    [InlineData("[()]}", false)]
+    public void Test(string input, bool output)
+    {
+        var result = ValidParentheses_Stack.Solution(input);
+
+        Assert.Equal(output, result);
+    }
+}
