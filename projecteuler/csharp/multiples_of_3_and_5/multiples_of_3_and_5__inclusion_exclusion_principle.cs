c19f529025257fc779fc27c023a51ac76eec91b9
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-18 23:30:45 +0300

    remove unnecessary files

--- projecteuler/csharp/multiples_of_3_and_5/multiples_of_3_and_5__inclusion_exclusion_principle.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProjectEuler.q1_multiples_of_3_and_5;
-
-public class MultiplesOf3And5_InclusionExclusionPrinciple
-{
-    public static int Solution(int n)
-    {
-        return 0;
-    }
-}
