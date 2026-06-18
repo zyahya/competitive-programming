512132aad5e3f259c655be41ffc321e89e809c3b
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-18 15:08:31 +0300

    refactor method name in test files for moving zeroes

--- codewars/csharp/kyu5/moving_zeroes_to_the_end/moving_zeroes_to_the_end_tests.cs
+++ codewars/csharp/kyu5/moving_zeroes_to_the_end/moving_zeroes_to_the_end_tests.cs
@@ -5,7 +5,7 @@ public class MovingZeroesToTheEndTests
     [Theory]
     [InlineData(new int[] { 4, 2, 4, 0, 0, 3, 0, 5, 1, 0 }, new int[] { 4, 2, 4, 3, 5, 1, 0, 0, 0, 0 })]
     [InlineData(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 }, new int[] { 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 })]
-    public void Test_Default_Solution(int[] input, int[] output)
+    public void TwoPointers(int[] input, int[] output)
     {
         var result = MovingZeroesToTheEnd__TwoPointers.Solution(input);
 
