f78d1a723fc22aab63fdc12733c50fc03c48214f
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-18 13:33:35 +0300

    Add test files

---
+++ projecteuler/csharp/q1_multiples_of_3_and_5/multiples_of_3_and_5_tests.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectEuler.q1_multiples_of_3_and_5;
+
+public class MultiplesOf3And5_Tests
+{
+    [Theory]
+    [InlineData(2, 0)]
+    [InlineData(10, 23)]
+    [InlineData(100, 2318)]
+    public void Test1(int input, int expectedResult)
+    {
+        var result = MultiplesOf3And5_BruteForce.Solution(input);
+
+        Assert.Equal(expectedResult, result);
+    }
+}
