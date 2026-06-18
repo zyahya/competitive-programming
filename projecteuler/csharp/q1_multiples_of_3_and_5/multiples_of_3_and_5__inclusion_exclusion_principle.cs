003be42c48c284e53caa7b8d2c408ad6ebfe32a2
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-18 12:59:39 +0300

    optimize 'multiples of 3 and 5' (inclusion exclusion principle solution)

---
+++ projecteuler/csharp/q1_multiples_of_3_and_5/multiples_of_3_and_5__inclusion_exclusion_principle.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectEuler.q1_multiples_of_3_and_5;
+
+public class MultiplesOf3And5_InclusionExclusionPrinciple
+{
+    public static int Solution(int n)
+    {
+        return 0;
+    }
+}
