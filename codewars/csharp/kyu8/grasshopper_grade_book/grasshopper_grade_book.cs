f0db12188b63ae0cd0c6111b84f7360a1fb67a7b
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-19 15:11:31 +0300

    fix 'grasshopper grade book' in C#

--- codewars/csharp/kyu8/grasshopper_grade_book/grasshopper_grade_book.cs
+++ codewars/csharp/kyu8/grasshopper_grade_book/grasshopper_grade_book.cs
@@ -4,7 +4,7 @@ public class GrasshopperGradeBook
 {
     public static char Solution(int s1, int s2, int s3)
     {
-        return ((double)(s1 + s2 + s3) / 30) switch
+        return ((s1 + s2 + s3) / 30) switch
         {
             10 => 'A',
             9 => 'A',
