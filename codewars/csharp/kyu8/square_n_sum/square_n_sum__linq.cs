458a062dd74679d109f3725fafeaaac6887ec75b
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-19 16:25:05 +0300

    refactor 'square n sum' (linq solution) in C#

--- codewars/csharp/kyu8/square_n_sum/square_n_sum__linq.cs
+++ codewars/csharp/kyu8/square_n_sum/square_n_sum__linq.cs
@@ -2,7 +2,7 @@ using System.Linq;
 
 namespace Codewars.Solutions.kyu8.square_n_sum;
 
-public class SquareNSum_Optimized
+public class SquareNSum_Linq
 {
     public static int Solution(int[] numbers)
     {
