99ddeae4401ddbb58eaf27032935555f66fa04c7
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-19 12:43:31 +0300

    solve 'area or perimeter' in C#

---
+++ codewars/csharp/kyu8/area_or_perimeter/area_or_perimeter_tests.cs
@@ -0,0 +1,12 @@
+namespace Codewars.Solutions.kyu8.area_or_perimeter;
+
+public class AreaOrPerimeterTests
+{
+    [Fact]
+    public void Test1()
+    {
+        // var result = AreaOrPerimeter.Solution();
+
+        // Assert.Equal();
+    }
+}
