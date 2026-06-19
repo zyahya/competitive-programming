98c62e03f0766673aa5acddf6d4042f09bc6932f
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-19 12:39:19 +0300

    solve '' in C#

---
+++ codewars/csharp/kyu8/area_or_perimeter/area_or_perimeter.cs
@@ -0,0 +1,9 @@
+namespace Codewars.Solutions.kyu8.area_or_perimeter;
+
+public class AreaOrPerimeter
+{
+    public static int Solution(int l, int w)
+    {
+        return l == w ? l * l : (l + w) * 2;
+    }
+}
