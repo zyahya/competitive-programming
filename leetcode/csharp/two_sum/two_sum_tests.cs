687d8351b338be908369cc4b0387fd77a1de5f66
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-26 10:18:57 +0300

    add test file for 'two sum' problem in C#

---
+++ leetcode/csharp/two_sum/two_sum_tests.cs
@@ -0,0 +1,15 @@
+namespace LeetCode.Solutions.two_sum;
+
+public class TwoSum_Tests
+{
+    [Theory]
+    [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+    [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+    [InlineData(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+    public void TwoSum_Solution_1(int[] nums, int target, int[] expectedResult)
+    {
+        int[] result = TwoSum_BruteForce.Solution(nums, target);
+
+        Assert.Equal(expectedResult, result);
+    }
+}
