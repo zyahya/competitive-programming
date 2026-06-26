cb2b417a10a8e73baccf40841fa5cb951efde63b
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-26 10:13:55 +0300

    solve 'two sum' using brute force in C#

---
+++ leetcode/csharp/two_sum/two_sum__brute_force.cs
@@ -0,0 +1,28 @@
+namespace LeetCode.Solutions.two_sum;
+
+public class TwoSum_BruteForce
+{
+    public static int[] Solution(int[] nums, int target)
+    {
+        int left = 0, right = left + 1;
+
+        while (left < nums.Length)
+        {
+            if (nums[left] + nums[right] == target)
+            {
+                return [left, right];
+            }
+
+            if (right == nums.Length - 1)
+            {
+                left++;
+                right = left + 1;
+                continue;
+            }
+
+            right++;
+        }
+
+        return [];
+    }
+}
