402e20d59d31ba28898a2a7752a2aee66c08289c
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-28 07:32:26 +0300

    add alternative linq solution for 'vowel count' in C#

---
+++ codewars/csharp/kyu7/vowel_count/vowel_count__linq.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+namespace Codewars.Solutions.kyu7.vowel_count;
+
+public class VowelCount_Linq
+{
+    public static int Solution(string str)
+    {
+        return str.Count("aeoui".Contains);
+    }
+}
